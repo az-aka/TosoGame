2c85417c7ff1ec09c4e3ddd504656f537cd19be4
az_aka <oswin.kazaki@gmail.com>
2022-10-30 23:07:40 +0900

    Initial commit

---
+++ src/main/java/aztosogame/aztosogame/AzTosoGame.java
@@ -0,0 +1,17 @@
+package aztosogame.aztosogame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AzTosoGame extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
