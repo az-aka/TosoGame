f6b61ab41bb27c75f512c3246c1b981e00e2014b
az_aka <oswin.kazaki@gmail.com>
2022-10-30 23:33:52 +0900

    イベントリスナーアイテム追加

--- src/main/java/aztosogame/aztosogame/EventListener.java
+++ src/main/java/aztosogame/aztosogame/EventListener.java
@@ -1,5 +1,6 @@
 package aztosogame.aztosogame;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -7,6 +8,6 @@ import org.bukkit.event.player.PlayerInteractEvent;
 public class EventListener implements Listener {
     @EventHandler
     public void onPlater(PlayerInteractEvent e){
-
+        Player p = e.getPlayer();
     }
 }
