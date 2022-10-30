bf00a3ca602dea39c4498f546c235b59e5847a33
az_aka <oswin.kazaki@gmail.com>
2022-10-30 23:32:56 +0900

    イベントリスナーテスト追記

--- src/main/java/aztosogame/aztosogame/EventListener.java
+++ src/main/java/aztosogame/aztosogame/EventListener.java
@@ -1,6 +1,12 @@
 package aztosogame.aztosogame;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class EventListener implements Listener {
+    @EventHandler
+    public void onPlater(PlayerInteractEvent e){
+
+    }
 }
